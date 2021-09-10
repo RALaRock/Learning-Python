3aac332b2bff5add99d118e27b770a467751eaef
RALaRock <ralarock@hotmail.com>
2021-09-09 21:09:23 -0700

    .

--- CrashCourse/chapter 15/15 generating data.py
+++ CrashCourse/chapter 15/15 generating data.py
@@ -1,10 +1,9 @@
 # data visualization - exploring data through visual representations
 # data analysis - using code to explore patterns and connections in a data set
 # Matplotlib - a mathematical plotting library
-# Plotly - generates visualizations that work well on digital devices by 
+# Plotly - generates visualizations that work well on digital devices by
 #   automatically resizing to fit a variety of display devices
 
 # installing Matplotlib
 
-#import matplotlib.pyplot as plt
-
+# import matplotlib.pyplot as plt
